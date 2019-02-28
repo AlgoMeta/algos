1f6e4ce46ffe09d503ad695fd87f50cf5d386792
AlgoMeta <AlgoMeta@users.noreply.github.com>
2019-02-28 17:56:44 -0400

    code

--- search.cpp
+++ search.cpp
@@ -22,57 +22,60 @@ int main()
   string search_key, input;
   int result;
 
-   cout<<"Welcome to \"search it\". We first need some input data."<<endl;
-   cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
-   cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+  cout<<"Welcome to \"search it\". We first need some input data."<<endl;
+  cout<<"We'll assume the inputs do not have any spaces."<<endl<<endl;
+  cout<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
 
-   cin>>input;
- 
-    while(input != "#")//read an unknown number of inputs from keyboard
-    {
-       inputs.push_back(input);
-       cin>>input;
-    }
+  cin>>input;
 
-   cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
+  while(input != "#")//read an unknown number of inputs from keyboard
+  {
+    inputs.push_back(input);
+    cin>>input;
+  }
+
+  cout<<endl<<"["<<inputs.size()<<" values read from input source]"<<endl<<endl;
 
   if(inputs.size() == 0)//no input
   {
-      cout<<endl<<"No input received, quiting..."<<endl<<endl;
-       exit(1);//nothing to do but quit program
+    cout<<endl<<"No input received, quiting..."<<endl<<endl;
+    exit(1);//nothing to do but quit program
   }
- 
-   cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
+
+  cout<<endl<<"To end input type the #-character (followed by Enter)"<<endl<<endl;
   cout<<"Enter a value to search for: ";
 
 
-   cin>>search_key;
- 
-    while(search_key != "#")//perform searches until sentinel entered
-    {
-        result = linearSearch(inputs,search_key);
+  cin>>search_key;
 
-        cout<<"  '"<<search_key<<"' was ";
+  while(search_key != "#")//perform searches until sentinel entered
+  {
+    result = linearSearch(inputs,search_key);
 
-        if (result == -1)
-          cout<<"not found";
-        else
-          cout<<"found at index "<<result;
+    cout<<"  '"<<search_key<<"' was ";
+
+    if (result == -1)
+    cout<<"not found";
+    else
+    cout<<"found at index "<<result;
 
 
-        cout<<endl<<endl<<"Enter a value to search for: ";
-        cin>>search_key; 
-    }
+    cout<<endl<<endl<<"Enter a value to search for: ";
+    cin>>search_key; 
+  }
 
-   cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
+  cout<<endl<<"Program \"search it\" is now finished."<<endl<<endl;
 
-    return 0;
+  return 0;
 }
 
 
-int linearSearch(auto data, auto key) {
-  for(auto x = 0; x < data.size(); x++){
-    if (data[x] == key){
+int linearSearch(auto data, auto key) 
+{
+  for(auto x = 0; x < data.size(); x++)
+  {
+    if (data[x] == key)
+    {
       return x;
     }
   }
